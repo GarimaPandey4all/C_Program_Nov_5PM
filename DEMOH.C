20c76f6f9cacec892d75ae3ad3827e95d537298f
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-11-29 18:22:50 +0530

    Add files via upload

---
+++ DEMOH.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//Write a program to print the Hello World on Output Screen.
+/*
+Pre-Processor Directive/Folder/Package
+#: Pre-Processor
+include
+Directive: <stdio.h>: Header File
+stdio: standard input/output
+*/
+//void: null or empty or nothing
+//function: ()
+void main()  // main(): Predefined Function, Entry point of every C program
+{
+	clrscr(); // clear screen
+	printf("Hello World"); // print the output on output screen
+	getch(); // get character
+}
---
+++ HMAYANK.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{       clrscr(); // clear screen
+	printf("Hello World");
+	getch(); // get character
+	return 0;
+}
